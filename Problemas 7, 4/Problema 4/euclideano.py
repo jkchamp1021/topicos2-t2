9899e3497de15bc8e474dba75af7b768e161c1bf
jkchamp1021 <47951126+jkchamp1021@users.noreply.github.com>
2020-06-06 12:46:14 -0500

    Verificar argumentos

--- Problemas 7, 4/Problema 4/euclideano.py
+++ Problemas 7, 4/Problema 4/euclideano.py
@@ -1,5 +1,6 @@
 import math #Libreria matematica
 import sys #Libreria de sistema
+ARG_NUM = 4 #Argumentos Permitidos
 class Puntos:
     def __init__(self, x, y): #Constructor
         self.x = x
@@ -10,9 +11,14 @@ class Puntos:
             d = math.sqrt(math.pow(punto_2.x - self.x, 2) + math.pow(punto_2.y - self.y, 2))
             return d
         except Exception as e:
-            print("A ocurrido un error ", e.__class__)
+            print("A ocurrido un error: ", e.__class__)
             exit(1)
+def VerifyArg(): #Numero adecuado de argumentos
+    return False if(len(sys.argv) - 1 != ARG_NUM) else True
 
+if(not VerifyArg()):
+    print("Incompatible cantidad de argumentos: Obtenidos - ", len(sys.argv) - 1, " de ", ARG_NUM)
+    exit(1)
 punto_1 = Puntos(1.5,2.3)
 punto_2 = Puntos(2,3)
 distancia = punto_1.DistanciaEuclidiana(punto_2)
