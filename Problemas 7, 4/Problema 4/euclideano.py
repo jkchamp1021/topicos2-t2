1c316fc86e79ed74a6db59406eb9dad000cd43e9
jkchamp1021 <47951126+jkchamp1021@users.noreply.github.com>
2020-06-06 14:00:28 -0500

    Reemplazado uso de arguentos, manejo csv

--- Problemas 7, 4/Problema 4/euclideano.py
+++ Problemas 7, 4/Problema 4/euclideano.py
@@ -1,6 +1,9 @@
 import math #Libreria matematica
 import sys #Libreria de sistema
-ARG_NUM = 4 #Argumentos Permitidos
+import csv #Libreria CSV reader/writter
+import time #Libreria de tiempo
+ARG_NUM = 0 #Argumentos Permitidos
+DELAY_PRINT = 0.5  #Delay de calculo e impresion
 class Puntos:
     def __init__(self, x, y): #Constructor
         self.x = x
@@ -11,17 +14,24 @@ class Puntos:
             d = math.sqrt(math.pow(punto_2.x - self.x, 2) + math.pow(punto_2.y - self.y, 2))
             return d
         except Exception as e:
-            print("A ocurrido un error: ", e.__class__)
+            print("A ocurrido un error: {}\n\n".format(e.__class__))
             exit(1)
 def VerifyArg(): #Numero adecuado de argumentos
-    return False if(len(sys.argv) - 1 != ARG_NUM) else True
+    return True if(len(sys.argv) - 1 == ARG_NUM) else False
 
-if(not VerifyArg()):
-    print("Incompatible cantidad de argumentos: Obtenidos - ", len(sys.argv) - 1, " de ", ARG_NUM)
-    exit(1)
-punto_1 = Puntos(1.5,2.3)
-punto_2 = Puntos(2,3)
-distancia = punto_1.DistanciaEuclidiana(punto_2)
-print("La distancia Euclidiana es: ", distancia)
+archivo = "euclid.csv" if(VerifyArg()) else sys.argv[1]
+
+with open(archivo,'rU') as csvf:
+    csv_archivo = csv.reader(csvf, delimiter=',', dialect=csv.excel_tab)
+    lineas = 0
+    for row in csv_archivo:
+        if not lineas == 0:
+            punto_1 = Puntos(float(row[0]), float(row[1]))
+            punto_2 = Puntos(float(row[2]), float(row[3]))
+            distancia = punto_1.DistanciaEuclidiana(punto_2)
+            print("Punto 1 [{} , {}] - Punto 2 [{} , {}] - La distancia Euclidiana es: {}\n\n\n".format(punto_1.x,punto_1.y,punto_2.x,punto_2.y,distancia))
+            #time.sleep(DELAY_PRINT)
+        lineas += 1
+    print("Calculado un total de {} lineas".format(lineas-1))
 
 
